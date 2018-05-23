54f5ae180394c9dfb4ceebc6a7664caeeb557e53
Scott Beaulieu <Scottb392@gmail.com>
2018-05-23 08:34:36 -0700

    Newer version in folder

--- Init.py
+++
@@ -1,8 +0,0 @@
-# uncompyle6 version 3.1.3
-# Python bytecode 3.6 (3379)
-# Decompiled from: Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)]
-# Embedded file name: Init.py
-Access_Token = ''
-Course_ID = ''
-Short_Course_ID = ''
-pageUrlHtmlList = []
